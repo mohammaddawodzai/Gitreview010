ccce8e6ab52c9dcc4a1743906e9135434812cdd2
mohammaddawodzai <mohammaddawodzai2022@gmail.com>
2023-02-25 02:39:27 -0500

    new commit4

---
+++ src/Main.java
@@ -0,0 +1,9 @@
+public class Main {
+    public static void main(String[] args) {
+
+        System.out.println("Hello world!");
+        System.out.println("Hello Batch15");
+        System.out.println("Hello again Batch15");
+        System.out.println("Hello one more time Batch15");
+    }
+}
